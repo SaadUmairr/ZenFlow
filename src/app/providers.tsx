ef1ad1eaf4c5b8fe8e397f05086cb2bb2bf37cfb
Saad Umair <saad.ansari0106@gmail.com>
2025-07-03 01:37:11 +0530

    Add smooth scroll

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from "react"
 import { JotaiInitializer } from "@/context/initializer"
 import { Provider as JotaiProvider } from "jotai"
+import { ReactLenis } from "lenis/react"
 
 import { ThemeProvider } from "@/components/theme-provider"
 
@@ -10,6 +11,7 @@ export function Providers({ children }: { children: ReactNode }) {
   return (
     <JotaiProvider>
       <ThemeProvider attribute="data-theme" defaultTheme="dark">
+        <ReactLenis root />
         <JotaiInitializer />
         {children}
       </ThemeProvider>
